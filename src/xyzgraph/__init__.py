d8bd7328f26c1b2aa4d0fe8ae6cf255e78141cba
James <144615809+JamesOBrien2@users.noreply.github.com>
2026-03-16 22:52:21 +0000

    Sort xyzgraph public imports

--- src/xyzgraph/__init__.py
+++ src/xyzgraph/__init__.py
@@ -8,49 +8,35 @@ __citation__ = (
     f"Coordinates, v{__version__}, 2025, https://github.com/aligfellow/xyzgraph.git."
 )
 
-# Data access
-# Comparison & visualization
 from .ascii_renderer import graph_to_ascii
 from .compare import compare_with_rdkit
 from .data_loader import DATA
-
-# Featurisers
 from .featurisers import compute_gasteiger_charges
-
-# Main API
 from .graph_builders import build_graph
 from .graph_builders_orca import build_graph_orca
 from .graph_builders_rdkit import build_graph_rdkit
 from .graph_builders_rdkit_tm import build_graph_rdkit_tm
 from .graph_builders_xtb import build_graph_xtb
-
-# NCI detection
 from .nci import NCIAnalyzer, NCIThresholds, detect_ncis
-from .stereo import annotate_stereo, assign_ez, assign_rs
-
-# ORCA support
 from .orca_parser import OrcaParseError, parse_orca_output
+from .stereo import annotate_stereo, assign_ez, assign_rs
 from .utils import count_frames_and_atoms, graph_debug_report, graph_to_dict, read_xyz_file
 
 __all__ = [
-    # Data access
     "DATA",
-    # NCI detection
     "NCIAnalyzer",
     "NCIThresholds",
-    # ORCA support
     "OrcaParseError",
-    # Main API
+    "annotate_stereo",
+    "assign_ez",
+    "assign_rs",
     "build_graph",
     "build_graph_orca",
     "build_graph_rdkit",
     "build_graph_rdkit_tm",
     "build_graph_xtb",
-    # Comparison & visualization
     "compare_with_rdkit",
-    # Featurisers
     "compute_gasteiger_charges",
-    # Utilities
     "count_frames_and_atoms",
     "detect_ncis",
     "graph_debug_report",
@@ -58,7 +44,4 @@ __all__ = [
     "graph_to_dict",
     "parse_orca_output",
     "read_xyz_file",
-    "assign_rs",
-    "assign_ez",
-    "annotate_stereo",
 ]
